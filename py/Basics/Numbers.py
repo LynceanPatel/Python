20347eb0ef6ba2cfbb3154399d08e3abc33f0c31
LynceanPatel <LynceanPatel@gmail.com>
2016-08-06 14:26:46 +0530

    Rename file from DataTypes to Numbers

---
+++ py/Basics/Numbers.py
@@ -0,0 +1,40 @@
+#3-Numbers
+''' The data stored in memory can be of many types. For example, a
+person's age is stored as a numeric value and his or her address is
+stored as alphanumeric characters. Python has various standard data
+types that are used to define the operations possible on them and the
+storage method for each of them.
+
+Python has five standard data types −
+
+Numbers
+
+String
+
+List
+
+Tuple
+
+Dictionary
+'''
+#Python Numbers
+'''
+Python supports four different numerical types −
+
+int (signed integers)
+
+float (floating point real values)
+
+complex (complex numbers)
+
+All integers in Python3 are represented as long integers. Hence there is no separate number type as long.
+'''
+
+number1 =10
+print("Number:",number1)
+
+#You can also delete the reference to a number object by using the del statement.
+del number1
+
+# TO hold the result on command prompt
+input("Press <<Enter>> to exit.")
